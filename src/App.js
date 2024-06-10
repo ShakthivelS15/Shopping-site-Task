14a8f9eaf607e4ced457ce9a9112d260b58c93d9
ShakthivelS15 <152275048+ShakthivelS15@users.noreply.github.com>
2024-06-10 21:46:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import CountdownTimer from "./newtimer";
+import Pages from './pages/pages'
+function App() {
+  return (<>
+    <Header />
+    <Pages/>
+    <Footer />
+  </>);
+}
+
+export default App;
